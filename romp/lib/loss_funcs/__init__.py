8ade758c8058d3b4d5dd1bf4c911cf8a36a8d5e4
MiGyeong Gwon <kmk0394@naver.com>
2024-05-20 14:14:33 +0900

    Add files via upload

---
+++ romp/lib/loss_funcs/__init__.py
@@ -0,0 +1,11 @@
+import sys, os
+lib_dir = os.path.dirname(__file__)
+root_dir = os.path.join(lib_dir.replace(os.path.basename(lib_dir),''))
+if root_dir not in sys.path:
+    sys.path.insert(0, root_dir)
+from .calc_loss import Loss
+from .learnable_loss import Learnable_Loss
+from .params_loss import batch_l2_loss, _calc_MPJAE
+from .keypoints_loss import batch_kp_2d_l2_loss, calc_mpjpe, calc_pampjpe, _calc_pck_loss, calc_pck, align_by_parts
+from .maps_loss import focal_loss, Heatmap_AE_loss, JointsMSELoss
+from .prior_loss import create_prior, MaxMixturePrior, L2Prior, SMPLifyAnglePrior, angle_prior
